15887de255e390fc2fd246e0d1d9d2f8a37643dc
ahmoin <123082059+ahmoin@users.noreply.github.com>
2025-08-10 13:05:28 -0400

    style: format image tag in signin page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -27,7 +27,13 @@ export default function SignIn() {
 			<div className="flex w-full max-w-sm flex-col gap-6">
 				<div className="flex items-center gap-2 self-center font-medium">
 					<div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-						<Image className="size-4" src="/salutis.svg" alt="Salutis logo" width={24} height={24} />
+						<Image
+							className="size-4"
+							src="/salutis.svg"
+							alt="Salutis logo"
+							width={24}
+							height={24}
+						/>
 					</div>
 					Salutis
 				</div>
