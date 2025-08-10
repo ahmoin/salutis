d842d35083daa4a2a7c8b9775801627cd02dddd7
ahmoin <123082059+ahmoin@users.noreply.github.com>
2025-08-10 11:11:09 -0400

    fix: hide error message and fix biome lint

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,3 +1,5 @@
+/** biome-ignore-all lint/a11y/noStaticElementInteractions: needed for navigation */
+/** biome-ignore-all lint/a11y/useKeyWithClickEvents: TODO: implement later*/
 "use client";
 
 import { useAuthActions } from "@convex-dev/auth/react";
@@ -22,12 +24,12 @@ export default function SignIn() {
 	return (
 		<div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
 			<div className="flex w-full max-w-sm flex-col gap-6">
-				<a href="#" className="flex items-center gap-2 self-center font-medium">
+				<div className="flex items-center gap-2 self-center font-medium">
 					<div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
 						{/* TODO: add logo here */}
 					</div>
 					Salutis
-				</a>
+				</div>
 				<div className="flex flex-col gap-6">
 					<Card>
 						<CardHeader className="text-center">
@@ -44,6 +46,7 @@ export default function SignIn() {
 										className="w-full"
 									>
 										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+											<title>Google</title>
 											<path
 												d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
 												fill="currentColor"
@@ -85,12 +88,12 @@ export default function SignIn() {
 										<div className="grid gap-3">
 											<div className="flex items-center">
 												<Label htmlFor="password">Password</Label>
-												<a
-													href="#"
+												{/* TODO: implement actual forgot password functionality */}
+												<div
 													className="ml-auto text-sm underline-offset-4 hover:underline"
 												>
 													Forgot your password?
-												</a>
+												</div>
 											</div>
 											<Input
 												id="password"
@@ -123,7 +126,7 @@ export default function SignIn() {
 					{error && (
 						<Label className="flex items-start gap-3 rounded-lg border p-3 border-red-900 bg-red-950">
 							<p className="text-foreground font-mono text-xs">
-								Error signing in: {error}
+								Could not sign in, did you mean to sign up?
 							</p>
 						</Label>
 					)}
